63348977a8c0402a1e124c1e5f68df7083ca52ed
eduardocv30 <jecruz@estudianTEC.cr>
2022-03-21 13:15:25 -0600

    Update Interfaz.java

--- Proyecto1/src/Interfaz.java
+++ Proyecto1/src/Interfaz.java
@@ -11,19 +11,6 @@ public class Interfaz extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new GridLayout(y,x));
         this.setVisible(true);
-        this.botones();
     }
-    public void botones() {
-        int i=0;
-        int j=0;
-        while(j<y){
-            Ficha ficha=new Ficha(i, j);
-            this.add(ficha.boton);
-            i++;
-            if (i==x){
-                i=0;
-                j++;
-            }
-        }
-    }
+   
 }
