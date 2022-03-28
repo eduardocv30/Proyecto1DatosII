919ff7fa1fa7e7fcb211e8fee567bbaa4bc95248
eduardocv30 <jecruz@estudianTEC.cr>
2022-03-28 10:50:31 -0600

    Update Ficha.java

--- Proyecto1DatosII/Proyecto1/src/Ficha.java
+++ Proyecto1DatosII/Proyecto1/src/Ficha.java
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> 0eb6127593fda1a79a815ef0bfb535d1f37e2783
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
@@ -9,12 +5,7 @@ import java.awt.event.ActionEvent;
 public class Ficha{
    private int i;
    private int j;
-<<<<<<< HEAD
    private String mensaje;
-=======
-   public static String mensaje;
-   public static boolean turno;
->>>>>>> 0eb6127593fda1a79a815ef0bfb535d1f37e2783
    public JButton boton;
 
    public Ficha(int i, int j){
@@ -24,15 +15,9 @@ public class Ficha{
        ActionListener evento=new ActionListener(){
            @Override
            public void actionPerformed(ActionEvent e){
-<<<<<<< HEAD
                Cliente c=new Cliente(getij());
                mensaje=c.getmensaje();
                boton.setText(mensaje);
-=======
-               turno=true;
-               Cliente c=new Cliente(getij());
-               mensaje=c.getmensaje();
->>>>>>> 0eb6127593fda1a79a815ef0bfb535d1f37e2783
            }
        };
        boton.addActionListener(evento);
@@ -42,10 +27,5 @@ public class Ficha{
        String retorno= String.valueOf(i)+String.valueOf(j);
        return retorno;
    }
-<<<<<<< HEAD
-=======
-
->>>>>>> 0eb6127593fda1a79a815ef0bfb535d1f37e2783
 }
-
 
