a62efcfc2cc2de51a0b2848f13181393aec944dd
eduardocv30 <jecruz@estudianTEC.cr>
2022-03-28 10:51:36 -0600

    Update main.java

--- Proyecto1DatosII/Proyecto1/src/main.java
+++ Proyecto1DatosII/Proyecto1/src/main.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -39,40 +39,3 @@ public class main {
             }
         }
     
-    
-
-=======
-import javax.swing.JButton;
-public class main {
-    public static void main(String[] args) {
-        Interfaz itf=new Interfaz();
-        int x=6;
-        int y=7;
-        int i=0;
-        int j=0;
-        JButton listaB[][]=new JButton[x][y];
-        while(j<y){
-            Ficha ficha=new Ficha(i, j);
-            itf.add(ficha.boton);
-            listaB[i][j]=ficha.boton;
-            i++;
-            if (i==x){
-                i=0;
-                j++;
-            }
-        }
-        while (true){
-            String mensaje = Ficha.mensaje;
-            //System.out.println("hola");
-            if(mensaje!=null){
-                int i1=Character.getNumericValue(mensaje.charAt(0));
-                int j1=Character.getNumericValue(mensaje.charAt(1));
-                listaB[i1][j1].setText((String.valueOf(mensaje.charAt(2))));
-                Ficha.falso();
-            }
-            
-        }
-    }
-    
-}
->>>>>>> 0eb6127593fda1a79a815ef0bfb535d1f37e2783
